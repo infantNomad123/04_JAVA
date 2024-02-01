f51e4eaffb4bed4c6486616bbc452611b4b3ab22
jkh <jkhwang58@gmail.com>
2024-02-01 17:27:57 +0900

    20240201_oop

--- 04_array/src/edu/kh/array/practice/PracticeRun.java
+++ 04_array/src/edu/kh/array/practice/PracticeRun.java
@@ -8,6 +8,11 @@ public class PracticeRun {
 //		ap.practice3();
 //		ap.practice4();
 //		ap.practice5();
-		ap.practice6();
+//		ap.practice6();
+//		ap.practice7();
+//		ap.practice8();
+//		ap.practice9();
+//		ap.practice10();
+		ap.practice11();
 	}
 }
